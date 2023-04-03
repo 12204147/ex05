f779b9e6b9ce9f523695ddc9b28287c6cc019a21
12204147 <gydlscksz123@naver.com>
2023-04-03 16:09:16 +0900

    ver01

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import {Row, Col, Table, Button} from 'react-bootstrap'
+import { Container, Row, Col, Table, Button } from 'react-bootstrap'
 
 const PostsPage = () => {
-  const [list, setList]=useState([]);
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [last, setLast] = useState(1);
@@ -10,50 +10,54 @@ const PostsPage = () => {
   const getPosts = () => {
     setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      let start=(page-1)*5 + 1;
-      let end=(page*5);
-      setList(json.filter(post=>post.id>=start && post.id<=end));
-      setLast(Math.ceil(json.length/5));
-      setLoading(false);
-  })
-}
-  useEffect(()=>{
+      .then(response => response.json())
+      .then(json => {
+        setLast(Math.ceil(json.length / 10));
+        let start = (page - 1) * 10 + 1;
+        let end = (page * 10);
+        setPosts(json.filter(post => post.id >= start && post.id <= end));
+        //setLast(Math.ceil(json.length / 5));
+        setLoading(false);
+      })
+  }
+  useEffect(() => {
     getPosts();
-  },[page]);
+  }, [page]);
 
-  if(loading) return <h1>로딩중입니다....</h1>
+  if (loading) return <h1 className='text-center my-5'>로딩중입니다....</h1>
   return (
-    <Row className='justify-content-center mx-3'>
-      <Col md={10}>
-        <h1 className='text-center mx-5'>게시글</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <td>ID.</td>
-              <td>Title</td>
-            </tr>
-          </thead>
-          <tbody>
-            {list.map(post=>
-              <tr key={post.id}>
-                <td>{post.id}</td>
-                <td>
-                  <div className='ellipsis'>{post.title}</div>
+    <Container>
+      <Row className='justify-content-center mx-3'>
+        <Col md={10}>
+          <h1 className='text-center my-5'>게시글</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <td>ID.</td>
+                <td>Title</td>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map(post =>
+                <tr key={post.id}>
+                  <td>{post.id}</td>
+                  <td>
+                    <div className='ellipsis'>{post.title}</div>
                   </td>
-              </tr>  
-            )}
-          </tbody>
-        </Table>
-        <div className='text-center my-3'>
-          <Button disabled={page===1 && true} onClick={()=>setPage(page-1)}>이전</Button>
-          <span className='px-3'>{page} / {last}</span>
-          <Button disabled={page===last && true} onClickk={()=>setPage(page+1)}>다음</Button>
-        </div>
-      </Col>
-    </Row>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+          <div class='text-center py-3'>
+            <Button disabled={page === 1 && true}
+            onClick={() => setPage(page - 1)}>이전</Button>
+            <span class='mx-3'>{page} / {last}</span>
+            <Button disabled={page === last && true}
+            onClick={() => setPage(page + 1)}>다음</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/components/TodosPage.jsx
+++ src/components/TodosPage.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Table, Button } from 'react-bootstrap'
 
-const TodosPage = () => {
+const PostsPage = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [last, setLast] = useState(1);
+
+  const getPosts = () => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {
+        setLast(Math.ceil(json.length / 10));
+        let start = (page - 1) * 10 + 1;
+        let end = (page * 10);
+        setPosts(json.filter(post => post.id >= start && post.id <= end));
+        //setLast(Math.ceil(json.length / 5));
+        setLoading(false);
+      })
+  }
+  useEffect(() => {
+    getPosts();
+  }, [page]);
+
+  if (loading) return <h1 className='text-center my-5'>로딩중입니다....</h1>
   return (
-    <div>
-        <h1>할일목록</h1>
-    </div>
+    <Container>
+      <Row className='justify-content-center mx-3'>
+        <Col md={10}>
+          <h1 className='text-center my-5'>할일목록</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <td>ID.</td>
+                <td>Title</td>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map(post =>
+                <tr key={post.id}>
+                  <td>{post.id}</td>
+                  <td>
+                    <div className='ellipsis'>{post.title}</div>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+          <div class='text-center py-3'>
+            <Button disabled={page === 1 && true}
+            onClick={() => setPage(page - 1)}>이전</Button>
+            <span class='mx-3'>{page} / {last}</span>
+            <Button disabled={page === last && true}
+            onClick={() => setPage(page + 1)}>다음</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
-export default TodosPage
+export default PostsPage
